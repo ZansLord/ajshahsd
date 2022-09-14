1bf5bd47ef3154af299e9daf976de25306a2c81d
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2022-09-14 15:54:57 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -744,7 +744,7 @@ function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
 
-global.thumb = 'https://telegra.ph/file/61f2d6d9694b49a2ce7aa.jpg'
+global.thumb = 'https://i.pinimg.com/originals/48/8a/75/488a75397c3df93aa4ff03713064e6bd.jpg'
 
 global.flaaa = [
  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
